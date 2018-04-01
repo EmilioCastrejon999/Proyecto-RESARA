ddd338c668e690215583d837bd3eba87cd7e9e43
Silvestre Castrejon <silvestre_castrejon@opindes.com>
2018-03-31 18:08:32 -0600

    fffff
    fdfddfd

--- prueba-sc.js
+++ prueba-sc.js
@@ -28,4 +28,39 @@ $('.tablinks').on('click', function(e) {
 
 $(window).bind("load",function(){
   $("#PrimerBoton").click(openPage(event, 'Page1'));
+
+  var mostrarEnAlerta;
+  if (checkCookie("Usuario")== true){
+  mostrarEnAlerta =  getCookie("Usuario");
+  }
+  else {
+    mostrarEnAlerta = "Inicia Sesion"
+  }
+
+  alert(mostrarEnAlerta)
 });
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(cname) {
+    var cookie = getCookie(cname);
+    if (cookie != "") {
+      return true;
+    } else {
+      return false;
+    }
+}
